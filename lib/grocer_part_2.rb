5f482aede653d499e4e3b554144f682b676d1731
Matthew Malecki <mmalecki4@gmail.com>
2020-05-03 20:00:49 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -89,13 +89,7 @@ def checkout(cart, coupons)
   new_cart = consolidate_cart(cart)
   new_cart = apply_coupons(new_cart)
   new_cart = apply_clearance
-binding.pry
-
-
-
- 
- 
-
+  binding.pry
 
 end
 
