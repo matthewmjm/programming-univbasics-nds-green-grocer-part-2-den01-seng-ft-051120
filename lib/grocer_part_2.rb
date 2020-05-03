6c2752890d12f7d1ec0a3ce9f1e7cd3173d1e995
Matthew Malecki <mmalecki4@gmail.com>
2020-05-03 16:08:05 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -5,26 +5,58 @@ def apply_coupons(cart, coupons)
   # definitions
   apply_coup_array = []
   item_cart = []
-  item_cart = consolidate_cart(cart)
+  with_coup = {}
+  item_cart = cart
   inx = 0
+  inx3 = 0
   
   #outer loop (goes through the coupons)
   coupons.each do |coup_item|
-    coup_item.each do |coup_key, coup_val|
-      
       #inner-loop (goes through the cart)
       inx2 = 0
       while inx2 < item_cart.length
-            
+        if coupons[inx][:item] == item_cart[inx2][:item]
+          #calculations
+          item_cart[inx2][:count]= item_cart[inx2][:count] - coupons[inx][:num]
+          item_cart[inx2][:count]= item_cart[inx2][:count]
+          #push to array
+          apply_coup_array = apply_coup_array.push(item_cart[inx2])
+          #calculations - item with coupon
+          updated_price = coupons[inx][:cost]/coupons[inx][:num]
+          updated_count = 
+          with_coup = {
+            :item => coupons[inx][:item].concat(" W/COUPON"),
+            :price => updated_price.round(2),
+            :clearance => item_cart[inx2][:clearance],
+            :count => coupons[inx][:num]
+          }
+          #push to array - item with coupon
+          apply_coup_array << with_coup
+        else
+        end
+       inx2 += 1     
       end
-      inx2 += 1
-    end
-    
     inx += 1
-  end 
-  
-binding.pry
+  end
+
+  #add non-coupon items to apply_coup_array
+  while inx3 < item_cart.length
+    inx4 = 0
+    while inx4 < apply_coup_array.length
+      if item_cart[inx3][:item] != apply_coup_array[inx4][:item]
+        apply_coup_array << item_cart[inx3]
+      else
+      end
+      inx4 +=1
+    end
+    inx3 += 1
+  end
+  apply_coup_array =apply_coup_array.uniq
+
+return apply_coup_array
 end
+
+
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
