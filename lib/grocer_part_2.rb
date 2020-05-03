26456cd12a9752b8ec962e45a9b4d1060d50f619
Matthew Malecki <mmalecki4@gmail.com>
2020-05-03 14:07:17 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,9 +1,29 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  # definitions
+  apply_coup_array = []
+  item_cart = []
+  item_cart = consolidate_cart(cart)
+  inx = 0
+  
+  #outer loop (goes through the coupons)
+  coupons.each do |coup_item|
+    coup_item.each do |coup_key, coup_val|
+      
+      #inner-loop (goes through the cart)
+      inx2 = 0
+      while inx2 < item_cart.length
+            
+      end
+      inx2 += 1
+    end
+    
+    inx += 1
+  end 
+  
+binding.pry
 end
 
 def apply_clearance(cart)
